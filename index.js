67697be7ca7a9ca1f7ce5b31eb10f933c2da42fb
Joshua Crehan <Joshua.Crehan@digitalcni.com>
2023-03-06 17:21:11 +0000

    completed manager code

--- index.js
+++ index.js
@@ -20,114 +20,129 @@ const { log } = require("console");
 // function to add the manager of the team
 function addManager() {
     inquirer
-    .prompt([    
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your id?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?'
-        },
-        {
-            type: 'input',
-            name: 'number',
-            message: 'What is your office number?'
-        },
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?'
+            },
+            {
+                type: 'input',
+                name: 'officeNum',
+                message: 'What is your office number?'
+            },
         ]).then((manager) => {
             console.log(manager);
-            const e = new Manager(manager);
-            console.log(e);        
+
+            const { name, id, email, officeNum } = manager;
+
+            const newManager = new Manager(name, id, email, officeNum);
+            console.log(newManager);
+            menuOption();
         });
-    };
+};
 
 // function fo menu option inquirer 
 function menuOption() {
     inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'options',
-            choices: ['Add Engineer', 'Add Intern', 'Complete the team'],
-            default: 'Complete the team'
-        }
-    ]).then((answers) => {
-
-    });
+        .prompt([
+            {
+                type: 'list',
+                name: 'options',
+                choices: ['Add Engineer', 'Add Intern', 'Complete the team'],
+                default: 'Complete the team'
+            }
+        ]).then((answer) => {
+            switch (answer) {
+                case 'Add Engineer':
+                    addEngineer();
+                    break;
+                case 'Add Intern':
+                    addIntern();
+                    break;
+                case 'Complete the team':
+                    completeTeam();
+                    break;
+            }
+        });
 };
 
 // function if user selects engineer
-function addEngineer(
+function addEngineer() {
     inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your id?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?'
-        },
-        {
-            type: 'input',
-            name: 'userName',
-            message: 'What is your GitHub username?'
-        }
-    ]).then((answers) => {
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?'
+            },
+            {
+                type: 'input',
+                name: 'userName',
+                message: 'What is your GitHub username?'
+            }
+        ]).then((answers) => {
 
-    });
-) {
-    
+        });
+
 };
 
 // function if user selects intern
 function addIntern() {
     inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your id?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?'
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'What is your school?'
-        }
-    ]).then((answers) => {
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?'
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is your school?'
+            }
+        ]).then((answers) => {
 
-    });
+        });
 };
 
 // function if user selects finish building team
-function completeTeam() {};
+function completeTeam() { };
 
 // function to initialize program
-function init() {}
+function init() {
+    addManager();
+}
 
 // function call to initialize program
 init();
