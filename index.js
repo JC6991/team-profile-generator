bd4763d42846cc7dad7e436b7a982af89698517d
Joshua Crehan <Joshua.Crehan@digitalcni.com>
2023-03-07 19:57:36 +0000

    completed all code

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ const { log } = require("console");
 
 // fs.writeFile(outputPath, data)
 
+// array that will hold all new employees
+let newTeam = [];
+
 // function to add the manager of the team
 function addManager() {
     inquirer
@@ -38,21 +41,26 @@ function addManager() {
             },
             {
                 type: 'input',
-                name: 'officeNum',
+                name: 'officeNumber',
                 message: 'What is your office number?'
             },
-        ]).then((manager) => {
-            console.log(manager);
+        ]).then((managerInput) => {
 
-            const { name, id, email, officeNum } = manager;
+            // sets the constructor parameters from the user input
+            const { name, id, email, officeNumber } = managerInput;
 
-            const newManager = new Manager(name, id, email, officeNum);
-            console.log(newManager);
+            // creates new manager employee using the "Manager" constructor
+            const manager = new Manager(name, id, email, officeNumber);
+
+            // push employee into newTeam array
+            newTeam.push(manager);
+
+            // reloads the menu to add more employees or finish creating team
             menuOption();
         });
 };
 
-// function fo menu option inquirer 
+// function for menu option inquirer 
 function menuOption() {
     inquirer
         .prompt([
@@ -63,7 +71,9 @@ function menuOption() {
                 default: 'Complete the team'
             }
         ]).then((answer) => {
-            switch (answer) {
+
+            // prompts user to select to add employees or finish creating team
+            switch (answer.options) {
                 case 'Add Engineer':
                     addEngineer();
                     break;
@@ -79,6 +89,7 @@ function menuOption() {
 
 // function if user selects engineer
 function addEngineer() {
+    // prompt for engineer to enter their information
     inquirer
         .prompt([
             {
@@ -98,17 +109,28 @@ function addEngineer() {
             },
             {
                 type: 'input',
-                name: 'userName',
+                name: 'gitHub',
                 message: 'What is your GitHub username?'
             }
-        ]).then((answers) => {
+        ]).then((engineerInput) => {
+            // sets the constructor parameters from the user input
+            const { name, id, email, gitHub } = engineerInput;
 
+            // creates new engineer employee using the "Engineer" constructor
+            const engineer = new Engineer(name, id, email, gitHub);
+
+            // push employee into newTeam array
+            newTeam.push(engineer);
+
+            // reloads the menu to add more employees or finish creating team
+            menuOption();
         });
 
 };
 
 // function if user selects intern
 function addIntern() {
+    // prompt for intern to enter their information
     inquirer
         .prompt([
             {
@@ -131,13 +153,32 @@ function addIntern() {
                 name: 'school',
                 message: 'What is your school?'
             }
-        ]).then((answers) => {
+        ]).then((internInput) => {
 
+            // sets the constructor parameters from the user input
+            const { name, id, email, school } = internInput;
+
+            // creates new intern employee using the "Intern" constructor
+            const intern = new Intern(name, id, email, school);
+
+            // push employee into newTeam array
+            newTeam.push(intern);
+
+            // reloads the menu to add more employees or finish creating team
+            menuOption();
         });
 };
 
 // function if user selects finish building team
-function completeTeam() { };
+function completeTeam() {
+    console.log('You have created your team.');
+
+    // function that passes the newTeam array to the generateTeam functionand creates HTML file
+    fs.writeFile(outputPath, render(newTeam), (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+
+};
 
 // function to initialize program
 function init() {
